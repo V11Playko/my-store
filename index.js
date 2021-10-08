4d65c1dc85fdc2552b051b5ba61a6e46f634f51f
V11Playko <heinnervega20@gmail.com>
2021-10-08 16:23:04 -0500

    Se elimino codigo obsoleto

--- index.js
+++ index.js
@@ -16,52 +16,6 @@ app.get('/nueva-ruta', (req, res) => {
 
 routerApi(app);
 
-/*
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });   
-  }
-  res.json(products);
-});
-
-
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Doritos',
-    price: 1600
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json ({
-      limit,
-      offset
-    });
-  } else {
-    res.send('No hay parametros');
-  }
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-})
-*/
 app.listen(port, () => {
   console.log('Mi port is: ' + port);
 });
