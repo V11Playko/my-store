9ca974f3984958c4df7b65cbe92733f3be42b078
V11Playko <heinnervega20@gmail.com>
2021-10-08 21:58:14 -0500

    Se hizo un deployment a heroku

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
 const whilelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
   origin: (origin, callback) => {
-    if (whilelist.includes(origin)) {
+    if (whilelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('No permitido'));
