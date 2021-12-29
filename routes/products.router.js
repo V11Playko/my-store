58d50c7b47038c6b240623d15c5fd0cb12473f0a
V11Playko <heinnervega20@gmail.com>
2021-12-29 08:09:05 -0500

    Filtrando precios con operadores

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,47 +1,54 @@
 const express = require('express');
 
-const ProductsService= require('./../services/product.service');
-const validatorHandler= require('./../middlewares/validator.handler');
-const { createProductSchema, updateProductSchema, getProductSchema }= require('./../schemas/product.schema');
+const ProductsService = require('./../services/product.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createProductSchema, updateProductSchema, getProductSchema, queryProductSchema } = require('./../schemas/product.schema');
 
 const router = express.Router();
 const service = new ProductsService();
 
-router.get('/', async (req, res) => {
-  const products = await service.find();
-  res.json(products);
-});
-
-router.get('/filter', (req, res) => {
-  res.send('Yo soy un filter');
-});
-
-router.get('/:id', 
-    validatorHandler(getProductSchema, 'params'),
-    async (req, res, next) => {
-      try {
-        const { id } = req.params;
-        const product = await service.findOne(id);
-        res.json(product);
-      } catch (error) {
-        next(error);
-      }
+router.get('/',
+  validatorHandler(queryProductSchema, 'query'),
+  async (req, res, next) => {
+    try {
+      const products = await service.find(req.query);
+      res.json(products);
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
-router.post('/', 
+router.get('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = await service.findOne(id);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/',
   validatorHandler(createProductSchema, 'body'),
-    async (req, res) => {
+  async (req, res, next) => {
+    try {
       const body = req.body;
       const newProduct = await service.create(body);
       res.status(201).json(newProduct);
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
-router.patch('/:id', 
+router.patch('/:id',
   validatorHandler(getProductSchema, 'params'),
   validatorHandler(updateProductSchema, 'body'),
-    async (req, res, next) => {
+  async (req, res, next) => {
     try {
       const { id } = req.params;
       const body = req.body;
@@ -52,10 +59,18 @@ router.patch('/:id',
     }
   }
 );
-router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
-  const rta = await service.delete(id);
-  res.json(rta);
-});
+
+router.delete('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({id});
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
--- services/product.service.js
+++ services/product.service.js
@@ -1,7 +1,7 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
 
-const sequelize = require('../libs/sequelize');
+const { models } = require('../libs/sequelize');
 
 class ProductsService {
 
@@ -24,18 +24,21 @@ class ProductsService {
   }
 
   async create(data) {
-    const newProduct = {
-      id: faker.datatype.uuid(),
-      ...data
-    }
-    this.products.push(newProduct);
+    const newProduct = await models.Product.create(data);
     return newProduct;
   }
 
-  async find() {
-    const query = 'SELECT * FROM tasks';
-    const [data] = await sequelize.query(query);
-    return data;
+  async find(query) {
+    const options = {
+      include: ['category'],
+    }
+    const { limit, offset } = query;
+    if (limit && offset) {
+      options.limit =  limit;
+      options.offset =  offset;
+    }
+    const products = await models.Product.findAll(options);
+    return products;
   }
 
   async findOne(id) {
