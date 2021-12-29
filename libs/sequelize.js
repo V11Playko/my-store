3dd962ea2592e02ddd5beb143f971d9799dd455c
V11Playko <heinnervega20@gmail.com>
2021-12-29 15:27:43 -0500

    Haciendo deploy en heroku

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -3,14 +3,20 @@ const { Sequelize } = require('sequelize');
 const { config } = require('./../config/config');
 const setupModels = require('./../db/models');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
-const sequelize = new Sequelize(URI, {
+const options = {
   dialect: 'postgres',
-  logging: false,
-});
+  logging: config.isProd ? false : true,
+}
+
+if (config.isProd) {
+  options.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+}
+
+const sequelize = new Sequelize(config.dbUrl, options);
 
 setupModels(sequelize);
 
