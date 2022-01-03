8c7b76e461ed28d461b7d10f79a4ba000900ac80
V11Playko <heinnervega20@gmail.com>
2022-01-03 16:57:56 -0500

    Middleware de verificacion

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
+const { checkApiKey } = require('./middlewares/auth.handler');
 
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whilelist = ['http://localhost:8080', 'https://myapp.co'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
   origin: (origin, callback) => {
-    if (whilelist.includes(origin) || !origin) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('No permitido'));
+      callback(new Error('no permitido'));
     }
   }
 }
-app.use(cors());
+app.use(cors(options));
 
 app.get('/', (req, res) => {
-  res.send('Hola mi gente bella');
+  res.send('Hola mi server en express');
 });
 
-app.get('/nueva-ruta', (req, res) => {
-  res.send('Llegue por otra ruta :v');
+app.get('/nueva-ruta', checkApiKey, (req, res) => {
+  res.send('Hola, soy una nueva ruta');
 });
 
 routerApi(app);
 
 app.use(logErrors);
+app.use(ormErrorHandler);
 app.use(boomErrorHandler);
-//app.use(errorHandler);
+app.use(errorHandler);
+
 
 app.listen(port, () => {
-  console.log('Mi port is: ' + port);
+  console.log(`Mi port ${port}`);
 });
